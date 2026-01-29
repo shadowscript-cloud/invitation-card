e21814c4919ad3db790ac3f37cb184cd650c72fd
shadowscript-cloud <shadowscript.cloud@gmail.com>
2026-01-29 18:14:33 +0530

    update-2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname)
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
