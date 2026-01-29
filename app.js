131b4c989a2c4e6d0cd84b5c9be8f5222559660c
shadowscript-cloud <shadowscript.cloud@gmail.com>
2026-01-29 17:57:48 +0530

    newupdate

---
+++ app.js
@@ -0,0 +1,23 @@
+const express =  require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname)
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.use(()=>{
+    console.log("WE got a new request")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`app is running on ${PORT}`);
+})
