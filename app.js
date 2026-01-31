d1e318d7573eae44b68218a3c5f56417339da48b
shadowscript-cloud <shadowscript.cloud@gmail.com>
2026-01-31 09:39:11 +0530

    update 3

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express =  require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.render('index.ejs')
 })
 
-app.use(()=>{
-    console.log("WE got a new request")
-})
+app.get('/:name/', (req, res) => {
+    const { name }= req.params; 
+    const fname = name.trim().split("-").join(" ").toUpperCase()
+    res.render('invite.ejs', {usrName : fname})
+});
 
+app.get(/(.*)/,(req,res)=>{
+    res.send("you landed a wrong page")
+})
 
 app.listen(PORT, ()=>{
     console.log(`app is running on ${PORT}`);
